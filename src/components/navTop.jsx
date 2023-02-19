aaf6682815c70d13ba82671a14940e7f93070022
Leonardo <leo11madara@gmail.com>
2023-02-19 12:12:06 -0300

    botando responsividade no navbar

--- src/components/navTop.jsx
+++ src/components/navTop.jsx
@@ -1,42 +1,60 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import CasasServices from '../services/casas.service';
 import './navTop.css';
-import CasasServices from '../services/casas.service'
+
 
 export default function navTop() {
 
-  const [casas, setCasas] = useState([])
-  const casasServices = new CasasServices()
+    const [casas, setCasas] = useState([])
+    const casasServices = new CasasServices()
 
-  useEffect(() => {
-    casasServices.get().then((res) => setCasas(res.data))
-  }, [])
+    useEffect(() => {
+        casasServices.get().then((res) => setCasas(res.data))
+    }, [])
 
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="https://fontmeme.com/fontes/fonte-game-of-thrones/">
-            <img
-              src="https://fontmeme.com/permalink/230216/b3840b887f5afc9f2bfc22bed4470942.png"
-              alt="fonte-game-of-thrones"
-              border="0"
-              height={50}
-              width={300}
-            />
-        </Navbar.Brand>
-        <Nav className="justify-content-end">
-          {casas.map(casa => {
-            return (
-              <Nav.Link key={casa.nome} href="#">
-                <img
-                  className='tamanho_emblema'
-                  src={`https://icons.iconarchive.com/icons/liav/game-of-thrones/128/${casa.nome}-icon.png`} />
-              </Nav.Link>
-            )
-          })}
+    return (
+        <>
+            {['md'].map((expand) => (
+                <Navbar key={expand} bg="dark" expand={expand} className="mb-3 navbar-dark">
+                    <Container fluid>
+                        <Navbar.Brand href="#"> <img
+                            src="https://fontmeme.com/permalink/230219/71b03d40502f0315c6ebab603c9c5c98.png"
+                            alt="fonte-game-of-thrones"
+                            border="0"
+                            height={50}
+                            width={300}
+                        /></Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                            bg="dark"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Principais casas de Westeros
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    {casas.map(casa => {
+                                        return (
+                                            <Nav.Link key={casa.nome} href="#">
+                                                <img
+                                                    className='tamanho_emblema'
+                                                    src={`https://icons.iconarchive.com/icons/limav/game-of-thrones/128/${casa.nome}-icon.png`} />
+                                            </Nav.Link>
+                                        )
+                                    })}
 
-        </Nav>
-      </Container>
-    </Navbar>
-  )
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </>
+    );
 }
--- src/services/casas.service.js
+++ src/services/casas.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default class CasasServices {
-  
+
 
   async get() {
     return axios.get('http://localhost:3000/casas');
