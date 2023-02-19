11423b5ef744ab915fcef0d7b6e78b08e3d1f3eb
Leonardo <leo11madara@gmail.com>
2023-02-19 14:06:49 -0300

    adicionando tooltips e menu hamburguer

--- src/components/navTop.jsx
+++ src/components/navTop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import { Container, Nav, Navbar, Offcanvas, OverlayTrigger, Tooltip, Row, Col } from 'react-bootstrap';
 import CasasServices from '../services/casas.service';
 import './navTop.css';
 
@@ -15,46 +15,67 @@ export default function navTop() {
 
     return (
         <>
-            {['md'].map((expand) => (
-                <Navbar key={expand} bg="dark" expand={expand} className="mb-3 navbar-dark">
-                    <Container fluid>
-                        <Navbar.Brand href="#"> <img
-                            src="https://fontmeme.com/permalink/230219/71b03d40502f0315c6ebab603c9c5c98.png"
-                            alt="fonte-game-of-thrones"
-                            border="0"
-                            height={50}
-                            width={300}
-                        /></Navbar.Brand>
-                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                        <Navbar.Offcanvas
-                            id={`offcanvasNavbar-expand-${expand}`}
-                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                            placement="end"
-                            bg="dark"
-                        >
-                            <Offcanvas.Header closeButton>
-                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                                    Principais casas de Westeros
-                                </Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body>
-                                <Nav className="justify-content-end flex-grow-1 pe-3">
+
+            <Navbar key='md' bg="dark" expand='md' className="mb-3 navbar-dark">
+                <Container fluid>
+                    <Navbar.Brand href="#"> <img
+                        src="https://fontmeme.com/permalink/230219/71b03d40502f0315c6ebab603c9c5c98.png"
+                        alt="fonte-game-of-thrones"
+                        border="0"
+                        height={50}
+                        width={300}
+                    /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls='offcanvasNavbar-expand-md' />
+                    <Navbar.Offcanvas
+                        id='offcanvasNavbar-expand-md'
+                        aria-labelledby='offcanvasNavbarLabel-expand-md'
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id='offcanvasNavbarLabel-expand-md'>
+                                Principais casas de Westeros
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <div>
                                     {casas.map(casa => {
                                         return (
-                                            <Nav.Link key={casa.nome} href="#">
+
+                                            <Nav.Link href="#" className="d-md-none">
                                                 <img
-                                                    className='tamanho_emblema'
+                                                    className='tamanho_emblema_hamburguer'
                                                     src={`https://icons.iconarchive.com/icons/limav/game-of-thrones/128/${casa.nome}-icon.png`} />
+                                                <span className="ms-2">{casa.nome}</span>
                                             </Nav.Link>
+
                                         )
                                     })}
+                                </div>
+                                <div className='d-flex flex-row'>
+                                    {casas.map(casa => {
+                                        return (
+                                            <OverlayTrigger
+                                                key={casa.nome}
+                                                placement="bottom"
+                                                delay={{ show: 250, hide: 400 }}
+                                                overlay={<Tooltip>{casa.nome}</Tooltip>}
+                                            >
+                                                <Nav.Link href="#" key={casa.nome} className="d-none d-md-block">
+                                                    <img
+                                                        className='tamanho_emblema'
+                                                        src={`https://icons.iconarchive.com/icons/limav/game-of-thrones/128/${casa.nome}-icon.png`} />
+                                                </Nav.Link>
+                                            </OverlayTrigger>
+                                        )
+                                    })}
+                                </div>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
 
-                                </Nav>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
-                    </Container>
-                </Navbar>
-            ))}
         </>
     );
 }
