594a20437226f490f7f0cdbe153f7e28ff135c2c
Leonardo <leo11madara@gmail.com>
2023-02-20 21:37:18 -0300

    adionando personagens

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ function App() {
       <NavTop />
 
       <div className='altura_max_bg alinhamento_flex' id='principal'>
-        <p className='titulo_principal'>Frases memoraveis</p>
+        
       </div>
+
 
       <div className='altura_max_bg alinhamento_flex' id='frase_nedStark'>
         <blockquote class="blockquote text-center">
@@ -21,9 +22,23 @@ function App() {
       </div>
 
       <div className='altura_max_bg alinhamento_flex' id='frase_tyrion'>
-        <blockquote class="blockquote text-center">
-          <p class="mb-4"><em>"Nunca esqueca o que voce e, o resto do mundo nao esquecera."</em></p>
+        <blockquote class="blockquote text-end">
+          <p class="mb-4"><em>"Nunca esqueca o que voce e, o resto do mundo nao esquecera. Faça disso a sua forca. Entao, nunca podera ser a sua fraqueza."</em></p>
           <footer class="blockquote-footer">Tyrion Lannister</footer>
+        </blockquote>
+      </div>
+
+      <div className='altura_max_bg alinhamento_flex' id='frase_arya'>
+        <blockquote class="blockquote text-start">
+          <p class="mb-4"><em>"O medo golpeia mais profundamente do que as espadas."</em></p>
+          <footer class="blockquote-footer">Arya Stark</footer>
+        </blockquote>
+      </div>
+
+      <div className='altura_max_bg alinhamento_flex' id='frase_daenerys'>
+        <blockquote class="blockquote text-start">
+          <p class="mb-4"><em>"Eu sou Daenerys Targaryen e eu vou pegar o que e meu com fogo e sangue."</em></p>
+          <footer class="blockquote-footer">Daenerys Targaryen</footer>
         </blockquote>
       </div>
     </>
